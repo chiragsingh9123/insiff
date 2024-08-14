1a6af0641cdbf779554abfe278c00453fd4a0a50
Chirag Singh <130963807+chiragsingh9123@users.noreply.github.com>
2024-08-15 01:32:29 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,7 +3,7 @@ import os
 
 def create_folder_and_upload(local_file_path1):
         source_folder = f'../insiff/{local_file_path1}'
-        destination_folder = '../../var/www/atlanta-api.online/scripts'
+        destination_folder = '/var/www/atlanta-api.online/scripts'
         if not os.path.exists(destination_folder+f"/{local_file_path1}"):
             shutil.move(source_folder, destination_folder)
             print(f"Successfully uploaded {local_file_path1} to {destination_folder}")
